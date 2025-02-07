f8c860d8847554107db24b777a80f3fd37364ae0
Nam Nguyen <nhhn1201@gmail.com>
2025-02-06 22:13:55 -0700

    Rerouted

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Import page components
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
-// Optionally, import About if you plan to use it:
-// import About from './pages/About';
-
-// Import shared components (if needed)
-// You might use Header and Footer here so they appear on every page
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-      {/* Optionally include a common header */}
       <Header />
-      
-      {/* Main routing area */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/booking" element={<Booking />} />
-        {/* Uncomment if you have an About page */}
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
-      
-      {/* Optionally include a common footer */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/booking" component={Booking} />
+        {/* Add other routes as needed */}
+      </Switch>
       <Footer />
     </Router>
   );
